486e2fb9b3c8f95f23603fbf0586e34637c7d295
Laura García González <laura.gagonzalez@alumnos.upm.es>
2026-09-01 14:03:13 +0200

    Add files via upload

---
+++ lab1/Ej6SqareSignal/Ej6SqareSignal/main.c
@@ -0,0 +1,23 @@
+/*
+ * Ej6SqareSignal.c
+ *
+ * Created: 01/09/2026 13:46:24
+ * Author : laura
+ */ 
+
+#define F_CPU 4915200UL   // Frequency of the connected crystal
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main(void)
+{
+	// PB0 as output 
+	DDRB |= (1 << PB0);
+
+	while (1)
+	{
+		PORTB ^= (1 << PB0);   // Toggle the pin state
+		_delay_ms(1);           // Adjust the delay to change the frequency
+	}
+}
